be036a82e9ad8e3683d860e9720e3b7db56062a8
Ethan Jaszewski <ethanjaszewski@yahoo.com>
2017-12-19 22:41:16 -0700

    Stripped down to absolute minimum.

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 from threading import Lock
-from flask import Flask, render_template, session, request
-from flask_socketio import SocketIO, emit, join_room, leave_room, \
-    close_room, rooms, disconnect
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, disconnect
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
@@ -22,47 +21,32 @@ def background_thread():
         socketio.sleep(10)
         count += 1
         socketio.emit('server_response',
-                      {'data': 'Server generated event', 'count': count},
+                      {'data': 'Server generated event'},
                       namespace='/socket')
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
-@socketio.on('event', namespace='/socket')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('server_response',
-         {'data': message['data'], 'count': session['receive_count']})
-
-@socketio.on('broadcast_event', namespace='/socket')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('server_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
-@socketio.on('disconnect_request', namespace='/socket')
-def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('server_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
-    disconnect()
-
 @socketio.on('ping', namespace='/socket')
 def ping_pong():
     emit('pong')
 
 @socketio.on('connect', namespace='/socket')
-def test_connect():
+def do_connect():
     global thread
     with thread_lock:
         if thread is None:
             thread = socketio.start_background_task(target=background_thread)
-    emit('server_response', {'data': 'Connected', 'count': 0})
+    emit('server_response', {'data': 'Connected'})
+
+@socketio.on('disconnect_request', namespace='/socket')
+def disconnect_request():
+    emit('server_response', {'data': 'Disconnected!'})
+    disconnect()
 
 @socketio.on('disconnect', namespace='/socket')
-def test_disconnect():
+def do_disconnect():
     print('Client disconnected', request.sid)
 
 if __name__ == '__main__':
