5110d1bb11efee336e9f37d63960a4c7d9e5423b
Ethan Jaszewski <ethanjaszewski@yahoo.com>
2018-01-17 23:02:07 -0800

    Finalized Message emissions scheme.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from threading import Lock
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, disconnect
 
+from messages import emit_message, ServerMessage
+
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
@@ -14,25 +16,26 @@ socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
+namespace = '/socket'
+
 def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
     while True:
         socketio.sleep(10)
         count += 1
-        socketio.emit('server_response',
-                      {'data': 'Server generated event'},
-                      namespace='/socket')
+        message = ServerMessage('Server generated event')
+        emit_message(socketio, message, namespace)
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
-@socketio.on('ping', namespace='/socket')
+@socketio.on('ping', namespace=namespace)
 def ping_pong():
     emit('pong')
 
-@socketio.on('connect', namespace='/socket')
+@socketio.on('connect', namespace=namespace)
 def do_connect():
     global thread
     with thread_lock:
@@ -40,12 +43,12 @@ def do_connect():
             thread = socketio.start_background_task(target=background_thread)
     emit('server_response', {'data': 'Connected'})
 
-@socketio.on('disconnect_request', namespace='/socket')
+@socketio.on('disconnect_request', namespace=namespace)
 def disconnect_request():
     emit('server_response', {'data': 'Disconnected!'})
     disconnect()
 
-@socketio.on('disconnect', namespace='/socket')
+@socketio.on('disconnect', namespace=namespace)
 def do_disconnect():
     print('Client disconnected', request.sid)
 
---
+++ messages.py
@@ -0,0 +1,39 @@
+def emit_message(socketio, message, namespace):
+    assert(isinstance(message, Message))
+    socketio.emit(message.message_type, message.data_map(), namespace=namespace)
+
+class Message:
+    message_type = None
+
+    def data_map(self):
+        raise NotImplementedError('This method must be implemented.')
+
+class SensorDataMessage(Message):
+    message_type = 'sensor_data'
+
+    def __init__(self, sensor_name, sensor_type):
+        self.sensor_name = sensor_name
+        self.sensor_type = sensor_type
+        self.sensor_value = None
+
+    def set_sensor_value(self, sensor_value):
+        self.sensor_value = sensor_value
+
+    def data_map(self):
+        return {
+            'data': 'Data from {}.'.format(self.sensor_name),
+            'sensor_name': self.sensor_name,
+            'sensor_type': self.sensor_type,
+            'sensor_value': self.sensor_value
+        }
+
+class ServerMessage(Message):
+    message_type = 'server_message'
+
+    def __init__(self, message):
+        self.message = message
+
+    def data_map(self):
+        return {
+            'data': self.message
+        }
