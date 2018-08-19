67004e189d873c31ae166a687a466a086b818937
Ethan Jaszewski <ethanjaszewski@yahoo.com>
2018-08-19 13:54:12 -0600

    Added template code for sending messages.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from threading import Lock
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, disconnect
-from flask_socketio import emit as sio_emit
+from flask_socketio import emit
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
@@ -11,6 +11,7 @@ async_mode = None
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+app.logger.disabled = True
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
@@ -21,5 +22,17 @@ namespace = '/socket'
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
+@socketio.on('cl_ping', namespace=namespace)
+def ping():
+    emit('sv_pong')
+
+@socketio.on('event', namespace=namespace)
+def event(msg):
+    print('EVENT_HANDLE:', msg)
+
+@socketio.on('broadcast_event', namespace=namespace)
+def broadcast_event(msg):
+    print('BROADCAST_EVENT_HANDLE', msg)
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=False)
