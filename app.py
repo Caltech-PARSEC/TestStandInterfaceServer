d227243604869a7ccff13c7894cbc41edeaf0695
Ethan Jaszewski <ethanjaszewski@yahoo.com>
2018-12-21 13:16:12 -0700

    Added client-server interfacing code.

--- app.py
+++ app.py
@@ -18,6 +18,24 @@ thread_lock = Lock()
 
 namespace = '/socket'
 
+# TODO: Valve & Sensor Managers
+valve_manager = None
+sensor_manager = None
+
+def client_tester():
+    while True:
+        # TODO: Add better client test
+        # Code to test Client
+        send_log_data('Hello World!')
+        socketio.sleep(1);
+
+@socketio.on('connect')
+def connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(client_tester)
+
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
@@ -26,17 +44,47 @@ def index():
 def ping():
     emit('sv_pong')
 
+####################
+# Message Handling #
+####################
 @socketio.on('cl_msg', namespace=namespace)
 def event(msg):
     print('Message from Client:', msg)
 
 @socketio.on('valve_seq', namespace=namespace)
-def event(valve_seq):
+def valve_seq(valve_seq):
     print('Valve Sequence:', valve_seq)
+
+@socketio.on('set_valve')
+def set_valve(data):
+    valve = valve_manager.get_valve(data['name'])
+    if data['angle'] == valve.open_angle:
+        valve.open()
+    else:
+        valve.close()
+
+@socketio.on('set_sensor')
+def set_sensor(data):
+    sensor = sensor_manager.get_sensor(data['name'])
+    sensor.set_sensor_value(data['value'])
 
 @socketio.on('emergency_stop', namespace=namespace)
 def emergency_stop():
     print('EMERGENCY STOP REQUESTED!!!')
+
+###################
+# Message Pushing #
+###################
+def send_sensor_data(sensor):
+    socketio.emit('sensor_data', {'name': sensor.get_name(),
+                                  'value': sensor.get_sensor_value()})
+
+def send_valve_data(valve):
+    socketio.emit('valve_data', {'name': valve.get_name(),
+                                 'angle': valve.last_value})
+
+def send_log_data(message):
+    socketio.emit('log_data', {'message': message})
 
 if __name__ == '__main__':
     socketio.run(app, debug=False)
